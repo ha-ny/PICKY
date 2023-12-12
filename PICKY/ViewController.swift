fb971ded030cdfe5f4d0206e452a0f7a367ffdfa
ha-ny <hany01.dev@gmail.com>
2023-12-13 02:27:16 +0900

    Initial Commit

---
+++ PICKY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PICKY
+//
+//  Created by 김하은 on 12/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
